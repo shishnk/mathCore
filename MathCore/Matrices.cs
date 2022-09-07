1aabc3c7e4d7c8b0a567cd661ef23e3888eaa385
shishnk <akorund@mail.ru>
2022-09-08 00:03:35 +0700

    Добавил документацию к классам матриц и вектора.

--- MathCore/Matrices.cs
+++ MathCore/Matrices.cs
@@ -1,5 +1,8 @@
 ﻿namespace MathCore;
 
+/// <summary>
+/// Class <c>Matrix</c> representing a rectangular matrix.
+/// </summary>
 public class Matrix<T> where T : INumber<T>
 {
     private readonly T[][] _storage;
@@ -12,13 +15,6 @@ public class Matrix<T> where T : INumber<T>
         set => _storage[i][j] = value;
     }
 
-    public Matrix(int size)
-    {
-        Rows = size;
-        Columns = size;
-        _storage = new T[size].Select(_ => new T[size]).ToArray();
-    }
-
     public Matrix(int rows, int columns)
     {
         Rows = rows;
@@ -26,6 +22,11 @@ public class Matrix<T> where T : INumber<T>
         _storage = new T[rows].Select(_ => new T[columns]).ToArray();
     }
 
+    /// <summary>
+    /// Method <c>Copy</c> with a new matrix object returned.
+    /// </summary>
+    /// <param name="otherMatrix">A matrix from which the values are copied.</param>
+    /// <returns>A copied matrix.</returns>
     public static Matrix<T> Copy(Matrix<T> otherMatrix)
     {
         Matrix<T> newMatrix = new(otherMatrix.Rows, otherMatrix.Columns);
@@ -42,6 +43,9 @@ public class Matrix<T> where T : INumber<T>
     }
 }
 
+/// <summary>
+/// Class <c>SquareMatrix</c> represents a square matrix.
+/// </summary>
 public class SquareMatrix<T> where T : INumber<T>
 {
     private readonly T[,] _storage;
@@ -60,24 +64,46 @@ public class SquareMatrix<T> where T : INumber<T>
         => Array.Clear(_storage, 0, _storage.Length);
 }
 
+/// <summary>
+/// Class <c>SparseMatrix</c> representing a sparse matrix.
+/// </summary>
 public class SparseMatrix
 {
-    // public fields - its bad, but the readability is better
-    public int[] ig;
-    public int[] jg;
-    public double[] di;
-    public double[] ggl;
-    public double[] ggu;
+    /// <value>Property <c>Ig</c> contains the indexes from which
+    /// the elements of the k-th row (column) begin in the arrays Jg, Ggl and Ggu.</value>
+    public int[] Ig { get; set; }
+
+    /// <value>Property <c>Jg</c> contains the numbers of columns (rows) of the stored off-diagonal
+    /// elements of the lower (upper) triangle matrix.</value>
+    public int[] Jg { get; set; }
+
+    /// <value>Property <c>Di</c> contains
+    /// the diagonal elements of the matrix.</value>
+    public double[] Di { get; set; }
+
+    /// <value>Property <c>Ggl</c> to store off-diagonal
+    /// elements of the lower triangle matrix by rows.</value>
+    public double[] Ggl { get; set; }
+
+    /// <value>Property <c>Ggu</c> to store off-diagonal
+    /// elements of the upper triangle matrix by columns.</value>
+    public double[] Ggu { get; set; }
+
     public int Size { get; }
 
+    /// <summary>
+    /// Constructor of sparse matrix.
+    /// </summary>
+    /// <param name="size">Matrix size.</param>
+    /// <param name="sizeOffDiag">Number of off-diagonal elements.</param>
     public SparseMatrix(int size, int sizeOffDiag)
     {
         Size = size;
-        ig = new int[size + 1];
-        jg = new int[sizeOffDiag];
-        ggl = new double[sizeOffDiag];
-        ggu = new double[sizeOffDiag];
-        di = new double[size];
+        Ig = new int[size + 1];
+        Jg = new int[sizeOffDiag];
+        Ggl = new double[sizeOffDiag];
+        Ggu = new double[sizeOffDiag];
+        Di = new double[size];
     }
 
     public static Vector<double> operator *(SparseMatrix matrix, Vector<double> vector)
@@ -86,30 +112,34 @@ public class SparseMatrix
 
         for (int i = 0; i < vector.Length; i++)
         {
-            product[i] = matrix.di[i] * vector[i];
+            product[i] = matrix.Di[i] * vector[i];
 
-            for (int j = matrix.ig[i]; j < matrix.ig[i + 1]; j++)
+            for (int j = matrix.Ig[i]; j < matrix.Ig[i + 1]; j++)
             {
-                product[i] += matrix.ggl[j] * vector[matrix.jg[j]];
-                product[matrix.jg[j]] += matrix.ggu[j] * vector[i];
+                product[i] += matrix.Ggl[j] * vector[matrix.Jg[j]];
+                product[matrix.Jg[j]] += matrix.Ggu[j] * vector[i];
             }
         }
 
         return product;
     }
 
+    /// <summary>
+    /// Output the sparse matrix in dense format.
+    /// </summary>
+    /// <param name="path">File path.</param>
     public void PrintDense(string path)
     {
         double[,] a = new double[Size, Size];
 
         for (int i = 0; i < Size; i++)
         {
-            a[i, i] = di[i];
+            a[i, i] = Di[i];
 
-            for (int j = ig[i]; j < ig[i + 1]; j++)
+            for (int j = Ig[i]; j < Ig[i + 1]; j++)
             {
-                a[i, jg[j]] = ggl[j];
-                a[jg[j], i] = ggu[j];
+                a[i, Jg[j]] = Ggl[j];
+                a[Jg[j], i] = Ggu[j];
             }
         }
 
@@ -125,20 +155,24 @@ public class SparseMatrix
         }
     }
 
+    /// <summary>
+    /// Conversion of the sparse matrix to the profile format.
+    /// Needed for direct solvers.
+    /// </summary>
     public void AsProfileMatrix()
     {
-        int[] ignew = ig.ToArray();
+        int[] ignew = Ig.ToArray();
 
         for (int i = 0; i < Size; i++)
         {
-            int i0 = ig[i];
-            int i1 = ig[i + 1];
+            int i0 = Ig[i];
+            int i1 = Ig[i + 1];
 
             int profile = i1 - i0;
 
             if (profile > 0)
             {
-                int count = i - jg[i0];
+                int count = i - Jg[i0];
                 ignew[i + 1] = ignew[i] + count;
             }
             else
@@ -157,14 +191,14 @@ public class SparseMatrix
 
             int j = i - (i1 - i0);
 
-            int i0Old = ig[i];
+            int i0Old = Ig[i];
 
             for (int ik = i0; ik < i1; ik++, j++)
             {
-                if (j == jg[i0Old])
+                if (j == Jg[i0Old])
                 {
-                    gglnew[ik] = ggl[i0Old];
-                    ggunew[ik] = ggu[i0Old];
+                    gglnew[ik] = Ggl[i0Old];
+                    ggunew[ik] = Ggu[i0Old];
                     i0Old++;
                 }
                 else
@@ -175,8 +209,8 @@ public class SparseMatrix
             }
         }
 
-        ig = ignew;
-        ggl = gglnew;
-        ggu = ggunew;
+        Ig = ignew;
+        Ggl = gglnew;
+        Ggu = ggunew;
     }
 }
